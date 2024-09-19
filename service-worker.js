6d4d21abc8dbff69bcd5000e215fd8a67a68a9d4
srg774 <75862558+srg774@users.noreply.github.com>
2024-09-19 10:07:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
 
+self.addEventListener('install', event => {
+    console.log('Service worker for Phantom Leap installing...');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker for Phantom Leap activating...');
+});
